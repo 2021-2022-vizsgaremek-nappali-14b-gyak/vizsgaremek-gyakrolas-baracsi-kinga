c7f4f1846cd063942156828c9bf628a99075e7b9
sz0barkin <sz0barkin@vasvari.org>
2022-01-13 09:02:21 +0100

    TDD összes teszt ViewModel rétegben megírva

---
+++ Desktop/src/VizsgaremekTests/ViewModels/DatabaseSourceViewModelTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vizsgaremek.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vizsgaremek.Models;
+
+namespace Vizsgaremek.ViewModels.Tests
+{
+    [TestClass()]
+    public class DatabaseSourceViewModelTests
+    {
+        [TestMethod()]
+        public void DatabaseSourceViewModelTest()
+        {
+            DatabaseSourceViewModel databaseSourceViewModel = new DatabaseSourceViewModel();
+            //felhasználó rákattintott a localhost szóra
+            databaseSourceViewModel.SelectedDatabaseSource = "localhost";
+            DbSource expectedDbSource = DbSource.LOCALHOST;
+
+            DbSource actualDbSource = databaseSourceViewModel.DbSource;
+
+            Assert.AreEqual(expectedDbSource, actualDbSource, "A kiválasztott adatbázis a 'localhost', de nem váltott át DbSource.LOCALHOST-ra");
+
+        }
+        [TestMethod()]
+        public void DatabaseSourceViewModelTestDevops()
+        {
+            DatabaseSourceViewModel databaseSourceViewModel = new DatabaseSourceViewModel();
+            //felhasználó rákattintott a localhost szóra
+            databaseSourceViewModel.SelectedDatabaseSource = "devops";
+            DbSource expectedDbSource = DbSource.DEVOPS;
+
+            DbSource actualDbSource = databaseSourceViewModel.DbSource;
+
+            Assert.AreEqual(expectedDbSource, actualDbSource, "A kiválasztott adatbázis a 'devops', de nem váltott át DbSource.DEVOPS-ra");
+        }
+        [TestMethod()]
+        public void DatabaseSourceViewModelTestNone()
+        {
+            DatabaseSourceViewModel databaseSourceViewModel = new DatabaseSourceViewModel();
+            //felhasználó rákattintott a localhost szóra
+            databaseSourceViewModel.SelectedDatabaseSource = "none";
+            DbSource expectedDbSource = DbSource.NONE;
+
+            DbSource actualDbSource = databaseSourceViewModel.DbSource;
+
+            Assert.AreEqual(expectedDbSource, actualDbSource, "A kiválasztott adatbázis a 'none', de nem váltott át DbSource.NONE-ra");
+        }
+    }
+}
